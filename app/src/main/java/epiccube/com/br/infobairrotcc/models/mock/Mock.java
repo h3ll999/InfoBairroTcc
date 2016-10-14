d2196bebe89f45846c8e713ae84875b69342ad1b
Anderson <andybadari@gmail.com>
2016-10-13 22:48:42 -0300

    6º commit - Ajustes mock

--- app/src/main/java/epiccube/com/br/infobairrotcc/models/mock/Mock.java
+++ app/src/main/java/epiccube/com/br/infobairrotcc/models/mock/Mock.java
@@ -16,14 +16,27 @@ public class Mock {
 
 
 
-        for (int i = 0; i<20000; i++){
-            Postagem p = new Postagem();
-            p.setTitulo("Comprei minha moto "+(i+1));
-            p.setConteudo("Ganhei na loteria e comprei uma moto");
-            p.setCategoria("Evento "+(i+1));
-            p.setNome("Mr. Mundiça "+(i+1));
-            p.setUrlPerfilUsuario("https://pbs.twimg.com/profile_images/689937066520625153/EKPQ5hNO.jpg");
-            postagens.add(p);
+        for (int i = 0; i<200; i++){
+
+            if (i%2==0){
+                Postagem p = new Postagem();
+                p.setTitulo("Comprei minha moto "+(i+1));
+                p.setConteudo("Ganhei na loteria e comprei uma moto");
+                p.setCategoria("Evento "+(i+1));
+                p.setNome("Mr. Mundiça "+(i+1));
+                p.setUrlPerfilUsuario("https://pbs.twimg.com/profile_images/689937066520625153/EKPQ5hNO.jpg");
+                postagens.add(p);
+            } else {
+                Postagem p = new Postagem();
+                p.setTitulo("Rua a ser asfaltada "+(i+1));
+                p.setConteudo("Uma rua ao lado da rua Das Colinas está precisando ser asfaltada porque está toda esburacada e isso pode fazer com que carros" +
+                        "caiam e explodam causando acidentes graves");
+                p.setCategoria("Atenção "+(i+1));
+                p.setNome("Mr. Mundiça "+(i+1));
+                p.setUrlPerfilUsuario("https://pbs.twimg.com/profile_images/689937066520625153/EKPQ5hNO.jpg");
+                postagens.add(p);
+            }
+
         }
 
         return postagens;
